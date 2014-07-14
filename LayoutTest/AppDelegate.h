dd9d14577769c1d9ac9159f6988e7bfa1bceed1e
pbuchheit <pbuchheit@tura.com>
2014-07-14 16:44:28 -0400

    initial checkin

---
+++ LayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutTest
+//
+//  Created by Patrick Buchheit on 7/14/14.
+//  Copyright (c) 2014 Patrick Buchheit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutTest
+//
+//  Created by Patrick Buchheit on 7/14/14.
+//  Copyright (c) 2014 Patrick Buchheit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
